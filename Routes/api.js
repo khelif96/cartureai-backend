015cc2dcae71f4111710464a3f17b13f9569e3a4
khelif96 <khelif96@gmail.com>
2018-04-21 17:06:34 -0400

    S3-uploads

--- Routes/api.js
+++ Routes/api.js
@@ -4,8 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 const apiHome = require('../Controllers/apiHome');
+const carData = require('../Controllers/carData');
+const s3Upload = require('../Controllers/s3Upload');
 
 router.get('/', apiHome.getApi);
 
+// router.get('/parseCarStats', carData.parseCarStats);
+
+router.post('/sign_s3', s3Upload.sign_s3 );
 router.use(apiHome.invalidPath);
 module.exports = router;
